eae3569d25ac1f23326cf7e4118024916aa6717d
Garik-Grafov <diomato@vk.com>
2024-11-09 18:03:00 +0300

    added several functions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let humanScore = 0;
+let computerScore = 0;
 // function for get computer choice
 function getComputerChoice() {
   let result = Math.random();
@@ -9,3 +11,18 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+// function for get user's choice
+function getHumanChoice() {
+  let result = prompt();
+  return result;
+}
+// function for play
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice == "rock" && computerChoice == "scissor") {
+    return computerScore++;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
